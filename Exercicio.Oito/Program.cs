0f9c1ab6009e63dad37a51cc8879a705a9124b79
gustavosalviato <gustavosalviato8@gmail.com>
2022-03-23 20:46:26 -0300

    Resolução Exercícios

---
+++ Exercicio.Oito/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercicio.Oito
+{
+    class Program
+    {
+        private const char _a = 'a';
+        private const char _e = 'e';
+        private const char _i = 'i';
+        private const char _o = 'o';
+        private const char _u = 'u';
+
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            try
+            {
+                Console.WriteLine($"Digite alguma palavra:");
+                var nome = Console.ReadLine().ToLower();
+
+                for (int i = 0; i < nome.Length; i++)
+                {
+                    if (_a.Equals(nome[i])
+                        || _e.Equals(nome[i])
+                        || _i.Equals(nome[i])
+                        || _o.Equals(nome[i])
+                        || _u.Equals(nome[i]))
+                        count++;
+                }
+
+                Console.WriteLine($"A palavra digitada possui {count} vogal(is)!");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Exercicio.Sete/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Exercicio.Sete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Informe seu nome");
+                var nome = Console.ReadLine();
+
+                Console.WriteLine("Informe sua data de nascimento");
+                DateTime.TryParse(Console.ReadLine(), out DateTime dataNascimento);
+
+                Console.WriteLine(ObterMensagemFaixaEtaria(nome, dataNascimento));
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+
+            }
+
+        }
+
+        private static string ObterMensagemFaixaEtaria(string nome, DateTime dataNascimento)
+        {
+            var idade = DateTime.Today.Year - dataNascimento.Year;
+
+            if (idade >= 60)
+                return $"Olá {nome}, está na faixa etária Idoso!";
+            else if (idade >= 20 && idade <= 59)
+                return $"Olá {nome}, está na faixa etária Adulto!";
+
+            return $"Olá {nome}, está na faixa etária Jovem!";
+        }
+    }
+}
