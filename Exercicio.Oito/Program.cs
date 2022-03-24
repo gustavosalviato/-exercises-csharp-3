513f9f1c858ca706bc8f7816a00f94c8892aedeb
gustavosalviato <gustavosalviato8@gmail.com>
2022-03-23 22:01:24 -0300

    Resolução Exercicio 02

--- Exercicio.Oito/Program.cs
+++ Exercicio.Oito/Program.cs
@@ -2,36 +2,41 @@
 
 namespace Exercicio.Oito
 {
-    class Program
-    {
-        private const char _a = 'a';
-        private const char _e = 'e';
-        private const char _i = 'i';
-        private const char _o = 'o';
-        private const char _u = 'u';
+    internal class Program
+    {   // readonly : acesso somente no construtor ou nova instancia do objeto
+        private static readonly Char[] _vogals = new []
+        {
+            'a', 'e', 'i', 'o', 'u',
+        };
+
+        //private const char _a = 'a';
+        //private const char _e = 'e';
+        //private const char _i = 'i';
+        //private const char _o = 'o';
+        //private const char _u = 'u';
 
         static void Main(string[] args)
         {
-            int count = 0;
 
             try
             {
-                Console.WriteLine($"Digite alguma palavra:");
+                Console.Write("Informe seu nome: ");
                 var nome = Console.ReadLine().ToLower();
+                var count = 0;
 
                 for (int i = 0; i < nome.Length; i++)
                 {
-                    if (_a.Equals(nome[i])
-                        || _e.Equals(nome[i])
-                        || _i.Equals(nome[i])
-                        || _o.Equals(nome[i])
-                        || _u.Equals(nome[i]))
-                        count++;
-                }
+                    var letra = nome[i];
 
-                Console.WriteLine($"A palavra digitada possui {count} vogal(is)!");
+                    if (_vogals.Contains(letra))
+                    {
+                        count++;
+                    }
+
+                }
+                Console.WriteLine($"Seu nome possui possui {count} consoantes!");
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
