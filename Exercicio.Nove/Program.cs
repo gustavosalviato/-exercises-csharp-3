8a01704df3e6eb6466a7a3b78fce802524b89a54
gustavosalviato <gustavosalviato8@gmail.com>
2022-03-23 22:34:11 -0300

    Resolução Exercicio 03

---
+++ Exercicio.Nove/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercicio.Nove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Livro("O reverso da medalha", "Sidney Sheldon", 100).ToString());
+        }
+    }
+}
